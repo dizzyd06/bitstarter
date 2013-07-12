5950124de7732685bf7519dac96f474b532cc26a
Daniel <demaestas@gmail.com>
2013-07-12 03:25:51 +0000

    trying the buf

--- web.js
+++ web.js
@@ -2,12 +2,13 @@
 var express = require('express');
 var fs = require ('fs');
 var buf = new Buffer(4);
-var fileData = fs.readFileSync('index.html');
+var fileData = fs.readFileSync('index.html','utf-8');
+buf.write(fileData);
 //document.write(fileData);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fileData);
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
