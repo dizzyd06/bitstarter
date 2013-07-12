6be510c690505814f2f12225fbd629a0865bf273
Daniel <demaestas@gmail.com>
2013-07-12 03:15:27 +0000

    fileData var added

--- web.js
+++ web.js
@@ -2,8 +2,8 @@
 var express = require('express');
 var fs = require ('fs');
 var buf = new Buffer(4);
-
-
+var fileData = fs.readFileSync('index.html');
+console.log(fileData);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
