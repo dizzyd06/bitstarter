c31d2700d2563306094bbe06cdcaaee1264559a2
Daniel <demaestas@gmail.com>
2013-07-13 01:54:15 +0000

    setting up http server in node

--- web.js
+++ web.js
@@ -1,17 +1,13 @@
+var http = require('http');
+var fs = require('fs');
 
-var express = require('express');
-var fs = require ('fs');
-var buf = new Buffer(28);
-var fileData = fs.readFileSync('index.html','utf-8');
-buf.write(fileData);
-//document.write(fileData);
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send(buf.toString('utf-8'));
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+fs.readFile('index.html',function (err, html) {
+    if(err){
+	throw err;
+    }
+    http.createServer(function(request, response) {
+	response.writeHeader(200, {"Content-Type": "text/html"});
+	response.write(html);
+	response.end();
+    }).listen(7000);
 });
