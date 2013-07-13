01b46dfa351d64747beffe8509baf165149fc752
Daniel <demaestas@gmail.com>
2013-07-13 02:09:32 +0000

    Changed app.get

--- web.js
+++ web.js
@@ -1,13 +1,18 @@
-var http = require('http');
-var fs = require('fs');
 
-fs.readFile('./index.html',function (err, html) {
-    if(err){
-	throw err;
-    }
-    http.createServer(function(request, response) {
-	response.writeHeader(200, {"Content-Type": "text/html"});
-	response.write(html);
-	response.end();
-    }).listen(7000);
+var express = require('express');
+var fs = require ('fs');
+//var buf = new Buffer(28);
+//var fileData = fs.readFileSync('index.html','utf-8');
+//buf.write(fileData);
+//document.write(fileData);
+var app = express.createServer(express.logger());
+
+app.get('index.html', function(request, response) {
+  //response.send(buf.toString('utf-8'));
+    response.end();
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
