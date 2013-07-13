ae9ef0ef3daf03e5ba31eefbf6de763419be3953
Daniel <demaestas@gmail.com>
2013-07-13 02:19:01 +0000

    added fileData as string :

--- web.js
+++ web.js
@@ -2,13 +2,14 @@
 var express = require('express');
 var fs = require ('fs');
 //var buf = new Buffer(28);
-//var fileData = fs.readFileSync('index.html','utf-8');
+var fileData = fs.readFileSync('index.html','utf-8');
 //buf.write(fileData);
 //document.write(fileData);
 var app = express.createServer(express.logger());
 
-app.get('/index.html', function(request, response) {
+app.get('/', function(request, response) {
   //response.send(buf.toString('utf-8'));
+    response.write(fileData);
     response.end();
 });
 
